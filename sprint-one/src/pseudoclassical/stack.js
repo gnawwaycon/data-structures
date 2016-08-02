4174732b02e69486e5852579002ed1c6d92e909d
Conway <conwaywang@gmail.com>
2016-08-02 08:43:30 -0700

    stack pseudocalssical implement

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.arr = [];
+  this.length = 0;
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
 };
-
-
+//
+//
+Stack.prototype.push = function(str){
+  this.arr.push(str);
+  this.length++;
+}
+//
+Stack.prototype.pop = function() {
+  if(this.length > 0) {
+    var temp = this.arr[this.length-1];
+    delete this.arr[--this.length];
+    return temp;
+  }
+}
+//
+Stack.prototype.size = function () {
+  return this.length;
+};
+//
+// var stack = new Stack();
