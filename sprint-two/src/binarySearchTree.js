22f125ff4c1d60d4a1011bec4fc9659e9f402ff3
conway <conwaywang@gmail.com>
2016-08-02 14:55:45 -0700

    complete linked list

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,18 @@
 var BinarySearchTree = function(value) {
+  
 };
 
+BinarySearchTree.prototype.insert = function(value){
 
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(){
+  
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if(list.head === null) {
+      list.head = list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var temp = this.head.value;
+    this.head = this.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
+    var containsVal = false;
+    var check = function(node) {
+      if(node.value === target) {
+        containsVal = true;
+      } else if(node.next !== null) {
+        check(node.next);
+      }
+    };
+    check(list.head);
+    return containsVal;
   };
 
   return list;
@@ -24,6 +44,8 @@ var Node = function(value) {
   return node;
 };
 
+var linkedList = new LinkedList;
+linkedList.addToTail()
 /*
  * Complexity: What is the time complexity of the above functions?
  */
